f0c1d5bb8f29a57dc2ee04a87ab31d063a0b16b3
Vanessa Dias <vds.vanessa94@gmail.com>
2021-12-21 15:21:19 -0300

    Aula 09 - Get e Before All

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -3,13 +3,19 @@ package plataformaFilmes;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlataformaFilmesTest {
 
-    public Response post (Object json, ContentType contentType, String endpoint){
+    static String token;
+
+    public static Response post(Object json, ContentType contentType, String endpoint){
         return RestAssured.given()
                 .relaxedHTTPSValidation()
                 .contentType(contentType)
@@ -18,6 +24,15 @@ public class PlataformaFilmesTest {
                 .post(endpoint)
                 .thenReturn();
 
+    }
+
+    private Response get(Map<String, String> header, String endpoint) {
+        return RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(header)
+                .when()
+                .get(endpoint)
+                .thenReturn();
     }
 
     @Test
@@ -32,9 +47,38 @@ public class PlataformaFilmesTest {
         Response response = post(json, ContentType.JSON, "auth");
 
         assertEquals(200, response.statusCode());
-        String token = response.jsonPath().get("token");
+        token = response.jsonPath().get("token");
 
 
     }
+
+    @BeforeAll
+    public static void validarLoginMap(){
+        RestAssured.baseURI = "http://localhost:8080/";
+        Map<String, String> map = new HashMap<>();
+        map.put("email", "aluno@email.com");
+        map.put("senha", "123456");
+
+        Response response = post(map, ContentType.JSON, "auth");
+
+        assertEquals(200, response.statusCode());
+        token = response.jsonPath().get("token");
+
+    }
+
+    @Test
+    public void validarConsultaCategorias(){
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", "Bearer " + token);
+
+        Response response = get(header, "categorias");
+
+        assertEquals(200, response.statusCode());
+
+        System.out.println(response.jsonPath().get().toString());
+
+    }
+
+
 
 }
